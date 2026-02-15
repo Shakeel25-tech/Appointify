c072e622bf20754aa68aa4cd7c222e1b79adc141
Mohd Shakeel <sahils20357@gmail.com>
2026-02-15 09:35:06 +0530

    register update done

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -3,7 +3,9 @@ import React from 'react'
 const Register = () => {
   return (
     <div>
-      <h1>Register Page</h1>
+      <h1>Register Page
+        hello world
+      </h1>
     </div>
   )
 }
