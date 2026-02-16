1705b1c468e88e7ad98a2e0bdeb1041efb31fbac
Mohd Shakeel <sahils20357@gmail.com>
2026-02-16 23:03:00 +0530

    form modified

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,12 +1,33 @@
 import React from 'react'
+import { Form } from 'antd'
 
 const Register = () => {
+
+  const onfinishHandler = (values) => {
+    console.log(values);
+  }
   return (
-    <div>
-      <h1>Register Page
-        hello world
-      </h1>
-    </div>
+    <>
+      <div className="form-container">
+        <Form layout="vertical" onFinish={onfinishHandler}>
+          <h1>Register Form</h1>
+          <Form.Item label="Name" name="name">
+            <input type="text" required/>
+          </Form.Item>
+          <Form.Item label="Email" name="email">
+            <input type="email" required/>
+          </Form.Item>
+          <Form.Item label="Password" name="password">
+            <input type="password" required/>
+          </Form.Item>
+          <Form.Item>
+            <button className='btn btn-primary' type='submit'>Register</button>
+          </Form.Item>
+
+        </Form>
+
+      </div>
+    </>
   )
 }
 
